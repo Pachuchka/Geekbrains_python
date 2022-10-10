3e1387e2a9396b04d9cf748109f1e4fc58fce375
Pachuchka <Pachuchka@gmail.com>
2022-10-10 21:25:04 +0800

    Первое домашнее задание, 7 задач

---
+++ Lesson_1_task_1.py
@@ -0,0 +1,6 @@
+print('задача 1')
+user_name = str(input('Введите ваши ФИО '))
+user_age = int(input('Введите ваш возраст '))
+user_mass = int(input('Введите ваш вес '))
+print(' вы ввели имя', user_name, ' возраст', str(user_age), ' и вес ', str(user_mass))
+
